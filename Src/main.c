ee1a6c03aac7eadb810a97aecd9c5efb17e197a2
dieguing <dfernandez@de.alten.com>
2020-03-12 23:25:24 +0100

    UART set, no interrupt, no clock set

--- Src/main.c
+++ Src/main.c
@@ -2,12 +2,26 @@
 #include "stm32l0xx.h"
 
 int main() {
+	/*	At this stage the microcontroller clock setting is already configured, 
+      this is done through SystemInit() function which is called from startup
+      file (startup_stm32l0xx.s) before to branch to application main.
+      To reconfigure the default setting of SystemInit() function, refer to
+      system_stm32l0xx.c file */
 
 	SystemConfig();
 
 	while(1){
 		/* Turn on the LED */
 		LED_ON();
+		USART2->TDR = 'O';
+		while((USART2->ISR & USART_ISR_TC) != USART_ISR_TC);
+		USART2->ICR = USART_ICR_TCCF;
+		USART2->TDR = 'K';
+		while((USART2->ISR & USART_ISR_TC) != USART_ISR_TC);
+		USART2->ICR = USART_ICR_TCCF;
+		USART2->TDR = 10;
+		while((USART2->ISR & USART_ISR_TC) != USART_ISR_TC);
+		USART2->ICR = USART_ICR_TCCF;
 		_delay();
 		LED_OFF();
 		_delay();
--- Src/sysconfig.c
+++ Src/sysconfig.c
@@ -5,9 +5,25 @@ void SystemConfig(){
 	/* Enable GPIOA clock */
 	RCC->IOPENR |= RCC_IOPENR_GPIOAEN;
 	/* Configure GPIOA pin 5 as output */
-	GPIOA->MODER = 0xEBFFF7FF;
+	GPIOA->MODER = (GPIOA->MODER & ~(GPIO_MODER_MODE5_1));
 	/* Configure GPIOA pin 5 in max speed */
 	GPIOA->OSPEEDR |= (3 << (LED_PIN * 2));
+	//PA2 Tx and PA3 Rx as Alternate Function	
+	GPIOA->MODER = (GPIOA->MODER & ~(GPIO_MODER_MODE2|GPIO_MODER_MODE3)) | (GPIO_MODER_MODE2_1|GPIO_MODER_MODE3_1);
+	//Configuration of Alternate Function register
+  GPIOA->AFR[0] = (GPIOA->AFR[0] & ~(GPIO_AFRL_AFSEL2|GPIO_AFRL_AFSEL3)) | ((4<<GPIO_AFRL_AFSEL2_Pos) | (4<<GPIO_AFRL_AFSEL3_Pos));
+	
+	/* Enable the peripheral clock USART2 */
+  RCC->APB1ENR |= RCC_APB1ENR_USART2EN;
+	/* Oversampling by 16, 9600 baud */
+	USART2->BRR = SystemCoreClock / 9600;
+	/* USART enable and Tx enable*/
+  USART2->CR1 = USART_CR1_TE|USART_CR1_UE;
+	
+	//while((USART2->ISR & USART_ISR_TC) != USART_ISR_TC);
+  //USART2->ICR = USART_ICR_TCCF; // clear TC flag transmission complete
+
+	
 }
 
 void _delay(){
